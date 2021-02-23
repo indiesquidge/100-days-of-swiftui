39c67ebdc3b420a50a79be688cbdfec7d3c5fc84
Austin Wood <i@austinwood.me>
2021-02-23 10:52:46 -0700

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by austin_wood on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
