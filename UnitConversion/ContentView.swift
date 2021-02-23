8f7acb258fbe90e1d4ba953b918be2aa703bfab7
Austin Wood <i@austinwood.me>
2021-02-23 11:07:43 -0700

    Implement length conversion using Measurement API

--- UnitConversion/ContentView.swift
+++ UnitConversion/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputValue = "1"
+    @State private var inputUnit = 1
+    @State private var outputUnit = 4
+
+    let units = ["meters", "kilometers", "feet", "yards", "miles", "centimeters"]
+
+    var unitSymbols: [String] {
+        units.map { unit in
+            (UnitLength.value(forKey: unit) as! UnitLength).symbol
+        }
+    }
+
+    var outputValue: Double {
+        let inputValue = Double(self.inputValue) ?? 0
+        let inputUnit  = units[self.inputUnit]
+        let outputUnit = units[self.outputUnit]
+
+        let inputMeasurement =
+            Measurement(value: inputValue, unit: UnitLength.value(forKey: inputUnit) as! UnitLength)
+        let baseMeasurement = inputMeasurement.converted(to: UnitLength.meters)
+        let outputMeasurement =
+            baseMeasurement.converted(to: UnitLength.value(forKey: outputUnit) as! UnitLength)
+
+        return outputMeasurement.value
+    }
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Input")) {
+                    TextField("Input value", text: $inputValue).keyboardType(.decimalPad)
+
+                    Picker("Output unit", selection: $inputUnit) {
+                        ForEach(0 ..< unitSymbols.count) { index in
+                            Text("\(self.unitSymbols[index])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+
+                Section(header: Text("Output")) {
+                    Text("\(outputValue)")
+
+                    Picker("Input unit", selection: $outputUnit) {
+                        ForEach(0 ..< unitSymbols.count) { index in
+                            Text("\(self.unitSymbols[index])")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+            }
+            .navigationBarTitle("Length Converter")
+        }
     }
 }
 
